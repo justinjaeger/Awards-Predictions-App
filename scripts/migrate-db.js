63f5a50d6c812a6c750b73c8b472844946850c2f
Justin Jaeger <jjustinjaeger@gmail.com>
2021-02-17 16:51:18 -0500

    created sql files with added tables for awards predictions

--- scripts/migrate-db.js
+++ scripts/migrate-db.js
@@ -30,30 +30,7 @@ async function query(q) {
 async function migrate() {
   try {
     await query(`
-    CREATE TABLE IF NOT EXISTS users (
-      user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      email VARCHAR(100) NOT NULL UNIQUE,
-      username VARCHAR(20) NOT NULL UNIQUE,
-      password VARCHAR(100) NOT NULL,
-      admin BIT(1) DEFAULT 0,
-      authenticated BIT(1) DEFAULT 0,
-      image VARCHAR(10000),
-      dateCreated DATETIME,
-      lastLoggedIn DATETIME
-    )
-
-    CREATE TABLE IF NOT EXISTS tokens (
-      access_token VARCHAR(250) NOT NULL PRIMARY KEY,
-      user_id INT NOT NULL,
-    )
-
-    CREATE TABLE IF NOT EXISTS followers (
-      username VARCHAR(20) NOT NULL,
-      follower VARCHAR(20) NOT NULL,
-      dateCreated DATETIME,
-      PRIMARY KEY (username, follower),
-      UNIQUE INDEX (username, follower)
-    )
+      // SQL instructions go here
     `)
     console.log('migration ran successfully')
   } catch (e) {
--- scripts/migrate-db_test.js
+++ scripts/migrate-db_test.js
@@ -30,31 +30,7 @@ async function query(q) {
 async function migrate() {
   try {
     await query(`
-    CREATE TABLE IF NOT EXISTS users (
-      user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      email VARCHAR(100) NOT NULL UNIQUE,
-      username VARCHAR(20) NOT NULL UNIQUE,
-      password VARCHAR(100) NOT NULL,
-      admin BIT(1) DEFAULT 0,
-      authenticated BIT(1) DEFAULT 0,
-      image VARCHAR(10000),
-      dateCreated DATETIME,
-      lastLoggedIn DATETIME
-    )
-
-    CREATE TABLE IF NOT EXISTS tokens (
-      access_token VARCHAR(250) NOT NULL PRIMARY KEY,
-      user_id INT NOT NULL,
-      username VARCHAR(20),
-    )
-
-    CREATE TABLE IF NOT EXISTS followships (
-      follower_id INT NOT NULL,
-      followee_id INT NOT NULL,
-      dateCreated DATE,
-      PRIMARY KEY (follower_id, followee_id),
-      UNIQUE INDEX (follower_id, followee_id)
-    );
+      // SQL instructions go here
     `)
     console.log('migration ran successfully')
   } catch (e) {
